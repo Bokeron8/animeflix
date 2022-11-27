704c7abdc00c52094f89df8d1eecbaffb5a2839e
Bokeron8 <francozambrano88@gmail.com>
2022-11-26 23:02:58 -0300

    Add config package

--- core/__init__.py
+++ core/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import os
-#from decouple import config
+from decouple import config
 from .apis import blueprint as api
 from core.views.routes import anime
 
 def create_app():
     app = Flask(__name__)
     
-    #app.config.from_object(config("APP_SETTINGS"))
+    app.config.from_object(config("APP_SETTINGS"))
     app.register_blueprint(api)
     app.register_blueprint(anime)
 
